b37c4b980fb6c64fc5cc1f911f53315ae7b3dfba
Adi Gunawan Silalahi <adigunawansilalahi@gmail.com>
2023-11-28 20:49:28 +0700

    Membuat insert users

--- app/views/users/input.php
+++ app/views/users/input.php
@@ -1,8 +1,8 @@
-<div class="card m-3">
+<div class="card m-3" style="background-color : #f8f8f8">
     <div class="card-header p-3">
         <h5>Input Users</h5>
     </div>
-    <form action="<?php echo URL; ?>/users/save" method="post" class="row g-3 mt-3 mb-3 p-3">
+    <form action="<?php echo URL; ?>/users/save" method="post" class="row g-3 mt-2 mb-3 p-3">
         <div class="col-md-6">
             <label class="form-label">Email</label>
             <input type="email" class="form-control" id="inputPassword4" name="email">
@@ -35,47 +35,8 @@
             <label class="form-label">Aktif</label>
             <input type="text" class="form-control" placeholder="Status" name="aktif">
         </div>
-        <button class="btn m-3" style="background-color : #eebbc3; color : #121629" type="submit" name="btn_save">Submit</button>
-
+        <div class="col-12 mt-3">
+            <button class="btn" style="background-color: #eebbc3; color: #121629" type="submit" name="btn_save">Submit</button>
+        </div>
     </form>
 </div>
-
-
-
-
-
-
-<!-- <h2>Input User</h2>
-
-<form action="<?php echo URL; ?>/users/save" method="post">
-    <table>
-        <tr>
-            <td>EMAIL</td>
-            <td><input type="email" name="email" required></td>
-        </tr>
-        <tr>
-            <td>PASSWORD</td>
-            <td><input type="password" name="password" required></td>
-        </tr>
-        <tr>
-            <td>NAMA</td>
-            <td><input type="text" name="name" required></td>
-        </tr>
-        <tr>
-            <td>ALAMAT</td>
-            <td><input type="text" name="alamat" required></td>
-        </tr>
-        <tr>
-            <td>NO HP</td>
-            <td><input type="text" name="hp" required></td>
-        </tr>
-        <tr>
-            <td>POS</td>
-            <td><input type="text" name="pos" required></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" name="btn_save" value="SAVE"></td>
-        </tr>
-    </table>
-</form> -->
