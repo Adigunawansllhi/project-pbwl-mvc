c873a943112c6484a3213f4efe10960d810b5f3b
Adi Gunawan Silalahi <adigunawansilalahi@gmail.com>
2023-11-28 20:44:25 +0700

    Melakukan perubahan desain di halaman edit users

--- app/views/users/edit.php
+++ app/views/users/edit.php
@@ -1,4 +1,4 @@
-<div class="card m-3">
+<div class="card m-3" style="background-color: #f8f8f8;">
     <div class="card-header p-3">
         <h5>Edit Users</h5>
     </div>
@@ -36,44 +36,8 @@
             <label class="form-label">Aktif</label>
             <input type="text" class="form-control" value="<?php echo $data['row']['user_aktif']; ?>" name="aktif">
         </div>
-        <button class="btn m-3" style="background-color : #eebbc3; color : #121629" type="submit" name="btn_update">Submit</button>
-
+        <div class="col-12 mt-3">
+            <button class="btn" style="background-color: #eebbc3; color: #121629;" type="submit" name="btn_update">Submit</button>
+        </div>
     </form>
 </div>
-
-
-<!-- <h2>Edit User</h2>
-
-<form action="<?php echo URL; ?>/users/update" method="post">
-    <input type="hidden" name="id" value="<?php echo $data['row']['user_id']; ?>">
-    <table>
-        <tr>
-            <td>EMAIL</td>
-            <td><input type="email" name="email" value="<?php echo $data['row']['user_email']; ?>" required></td>
-        </tr>
-        <tr>
-            <td>PASSWORD</td>
-            <td><input type="password" name="password"></td>
-        </tr>
-        <tr>
-            <td>NAMA</td>
-            <td><input type="text" name="name" value="<?php echo $data['row']['user_name']; ?>" required></td>
-        </tr>
-        <tr>
-            <td>ALAMAT</td>
-            <td><input type="text" name="alamat" value="<?php echo $data['row']['user_alamat']; ?>" required></td>
-        </tr>
-        <tr>
-            <td>NO HP</td>
-            <td><input type="text" name="hp" value="<?php echo $data['row']['user_hp']; ?>" required></td>
-        </tr>
-        <tr>
-            <td>POS</td>
-            <td><input type="text" name="pos" value="<?php echo $data['row']['user_pos']; ?>" required></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" name="btn_update" value="UPDATE"></td>
-        </tr>
-    </table>
-</form> -->
