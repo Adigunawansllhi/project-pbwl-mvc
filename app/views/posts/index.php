4323e111c13781787a0d9f76c5dd48a76d6027ea
Adi Gunawan Silalahi <adigunawansilalahi@gmail.com>
2023-11-28 20:42:03 +0700

    Mengedit tampilan halaman pelanggan

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -7,6 +7,7 @@
                   <th>NO</th>
                   <th style="width : 100px;">GOLONGAN</th>
                   <th>NAMA</th>
+                  <th>NO PELANGGAN</th>
                   <th>ALAMAT</th>
                   <th>NO HP</th>
                   <th>NO KTP</th>
@@ -24,6 +25,7 @@
                         <td><?php echo $no; ?></td>
                         <td><?php echo $row['gol_name']; ?></td>
                         <td><?php echo $row['pel_name']; ?></td>
+                        <td><?php echo $row['pel_no']; ?></td>
                         <td><?php echo $row['pel_alamat']; ?></td>
                         <td><?php echo $row['pel_hp']; ?></td>
                         <td><?php echo $row['pel_ktp']; ?></td>
@@ -31,9 +33,9 @@
                         <td><?php echo $row['pel_meteran']; ?></td>
                         <td><?php echo $row['pel_aktif']; ?></td>
                         <td><?php echo $row['user_name']; ?></td>
-                        <td class="d-flex">
-                              <a href="<?php echo URL; ?>/posts/edit/<?php echo $row['gol_id']; ?>" class="btn btn-info"><i class="fas fa-edit"></i></a>
-                              <a href="<?php echo URL; ?>/posts/delete/<?php echo $row['gol_id']; ?>" class="btn btn-danger" onclick="return confirm('Are you sure?')"><i class="fas fa-trash"></i></a>
+                        <td class="d-flex gap-2">
+                              <a href="<?php echo URL; ?>/posts/edit/<?php echo $row['pel_id']; ?>" class="btn btn-info"><i class="fas fa-edit"></i></a>
+                              <a href="<?php echo URL; ?>/posts/delete/<?php echo $row['pel_id']; ?>" class="btn btn-danger" onclick="return confirm('Are you sure?')"><i class="fas fa-trash"></i></a>
                         </td>
                   </tr>
             <?php $no++;
