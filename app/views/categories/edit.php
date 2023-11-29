4b25eaf4c69ab4ccdb9f515b6c95f610e2c03a1c
Adi Gunawan Silalahi <adigunawansilalahi@gmail.com>
2023-11-29 15:28:22 +0700

    Merapikan tampilan edit golongan

--- app/views/categories/edit.php
+++ app/views/categories/edit.php
@@ -1,9 +1,9 @@
 <div class="card col-10 m-3">
     <div class="card-header">
-        <h5>Input Golongan</h5>
+        <h5>Edit Golongan</h5>
         <form action="<?php echo URL; ?>/categories/update" method="post" class="mt-3" enctype="multipart/form-data">
 
-        <input type="hidden" name="id" value="<?php echo isset($data['row']['gol_id']) ? $data['row']['gol_id'] : ''; ?>">
+            <input type="hidden" name="id" value="<?php echo isset($data['row']['gol_id']) ? $data['row']['gol_id'] : ''; ?>">
 
 
             <div class="mb-3">
@@ -24,22 +24,3 @@
 
 
 
-<!-- <h2>Edit Golongan</h2>
-
-<form action="<?php echo URL; ?>/categories/update" method="post">
-    <input type="hidden" name="id" value="<?php echo $data['row']['gol_id']; ?>">
-    <table>
-    <tr>
-            <td>KODE</td>
-            <td><input type="text" name="gol_kode" value="<?php echo $data['row']['gol_kode']; ?>" required></td>
-        </tr>
-        <tr>
-            <td>NAME</td>
-            <td><input type="text" name="gol_name" value="<?php echo $data['row']['gol_name']; ?>" required></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" name="btn_update" value="UPDATE"></td>
-        </tr>
-    </table>
-</form> -->
